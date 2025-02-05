0f89acc06f6292508ac9e2c270dc6577853639ed
Jason Ziegler <jason@insightfulautomation.com>
2025-02-04 20:27:31 -0600

    move functions to top of js file

--- main.js
+++ main.js
@@ -1,38 +1,53 @@
 // This is the main js file
 
-
-
-
-
-
+// hard code a map in a 2d array
 let map = [
   ["#", "#", "#", "#"],
   ["#", ".", ".", "#"],
-  ["#", "@", ".", "#"],
+  ["#", ".", "a", "#"],
   ["#", "#", "#","#"],
 ];
 
+// copy the map
+// need to be able to reference the layout of the map
+// the monsters and player will walk over the map
+// some items need to stay on the map even if the 
+// character walks over it
+let mapCopy = JSON.parse(JSON.stringify(map));
+
 // [0,1,2]
+// get length and width of map for looping over map
 const MAX_Y = map.length;
 const MAX_X = map[0].length;
 
-let map_string = "";
-
-for (let i = 0; i < map.length; i++) {
-  for (let j = 0; j < map[i].length; j++) {
-    map_string = map_string + map[i][j];
+// this function will turn a map into a string to be
+// used in an HTML <pre> block
+// takes a 2d map array
+// returns a string
+function generateMapLayout(map) {
+  let map_string = "";
+  for (let i = 0; i < map.length; i++) {
+    for (let j = 0; j < map[i].length; j++) {
+      map_string = map_string + map[i][j];
+    }
+    map_string = map_string + "\n";
   }
-  map_string = map_string + "\n";
+  return map_string;
 }
 
-function drawMap() {
-  document.getElementById('map-display').innerText = map_string;
+// this function prints a map in a browser window
+// accepts 1 argument: string 
+function printMap(layout) {
+  document.getElementById('map-display').innerText = layout;
 }
 
-console.log(map_string);
+// place player token
+function placePlayerToken(map, {x,y}) {
+  map[x][y] = "@";
+}
 
-drawMap();
-
+//locatePlayer finds the player token on a given map
+// accepts 2 arguments: string, array (2d)
 function locatePlayer(playerChar, map) {
   // loop through 2d array
   console.log(playerChar);
@@ -45,66 +60,97 @@ function locatePlayer(playerChar, map) {
           return {x: j, y: i};
         }
     }
-    
   }
   return false;
 }
-console.log(map[2][1]);
-let playerLocation = locatePlayer("@", map);
 
-console.log(JSON.stringify(playerLocation));
-
-addEventListener('keydown', function (e) {
-
-let direction = "";
-if (e.key === "w" | e.key === "ArrowUp") {movePlayer(playerLocation,"up")}
-if (e.key === "a" | e.key === "ArrowLeft") {movePlayer(playerLocation,"left")}
-if (e.key === "s" | e.key === "ArrowDown") {movePlayer(playerLocation,"down")}
-if (e.key === "d" | e.key === "ArrowRight") {movePlayer(playerLocation,"right")}
-
-console.log(direction);
-})
-
-
+// movePlayer moves the player token on the mapCopy
+// acceptes two arguments: object {x,y}, string
 function movePlayer(playerLocation, direction) {
   // determine target location
-  let targetLocation = playerLocation;
+  let targetLocation = {...playerLocation};
   // // target location is player location + direction
   // // up inc y axis
   // // left dec x axis
   // // down dec y axis
   // // right inc x axis
   if (direction === "up") {
-    targetLocation.y--;
-  }
-  if (direction === "left") {
     targetLocation.x--;
   }
-  if (direction === "down") {
-    targetLocation.y++;
+  if (direction === "left") {
+    targetLocation.y--;
   }
-  if (direction === "right") {
+  if (direction === "down") {
     targetLocation.x++;
   }
-  console.log(targetLocation);
+  if (direction === "right") {
+    targetLocation.y++;
+  }
+   console.log(targetLocation);
 
-// check move target location is valid
-let locationValue = map[targetLocation.x][targetLocation.y];
+  // // target location is not out of bounds
+  if (targetLocation.x < 0 
+    || targetLocation.x > MAX_X 
+    || targetLocation.y < 0 
+    || targetLocation.y > MAX_Y
+  ) {
+    return console.log("OUT OF BOUNDS");
+  } else {
+    console.log(targetLocation);
+  }
 
+  let targetLocationValue = map[targetLocation.x][targetLocation.y];
+  console.log (targetLocationValue);
 
-// // target location is not out of bounds
-if (targetLocation.x < 0 
-  || targetLocation.x > MAX_X 
-  || targetLocation.y < 0 
-  || targetLocation.y > MAX_Y
-) {
-  console.log("OUT OF BOUNDS");
-} else {
-  console.log(targetLocation);
+  // // target location is not a wall
+  if (targetLocationValue === "#") {
+    return (console.log("Wall"));
+  }
+  // // target location is not a closed door
+  if (targetLocationValue === "D") {
+    return (console.log("Door"));
+  }
+  // // target location is not a monster
+  if (targetLocationValue === "a") {
+    return (console.log("Arachnid"));
+  }
+  // check move target location is valid
+  // if valid check if empty space
+  if (targetLocationValue === ".") {
+    console.log("The way is clear.");
+    // then move player to new location
+    // store value of player location from original map
+    playerLocationGroundValue = map[playerLocation.x][playerLocation.y];
+    // replace the current player location on map Copy with the new value
+    mapCopy[playerLocation.x][playerLocation.y] = playerLocationGroundValue;
+    // place character symbol at target location on map Copy
+    printMap(generateMapLayout(mapCopy));
+    placePlayerToken(mapCopy,{x:targetLocation.x, y:targetLocation.y});
+    printMap(generateMapLayout(mapCopy));
+    return;
+  }
 }
-// // target location is not a wall
-// // target location is not a closed door
-// // target location is not a monster
+let mapLayout = generateMapLayout(mapCopy);
+console.log(mapLayout);
+printMap(mapLayout);
+placePlayerToken(mapCopy, {x: 1,y: 1});
+printMap(generateMapLayout(mapCopy));
+// setTimeout(({x=1,y=1}) => placePlayerToken, 1000);
+//console.log(map_string);
+console.log(map[2][1]);
+let playerLocation = locatePlayer("@", mapCopy);
+console.log("PLayer Location: ", JSON.stringify(playerLocation));
 
+// Event listener for player movement
+addEventListener('keydown', function (e) {
 
-}
+let direction = "";
+
+// TODO: Player location not being updated after playerMove
+if (e.key === "w" | e.key === "ArrowUp") {movePlayer(playerLocation,"up")}
+if (e.key === "a" | e.key === "ArrowLeft") {movePlayer(playerLocation,"left")}
+if (e.key === "s" | e.key === "ArrowDown") {movePlayer(playerLocation,"down")}
+if (e.key === "d" | e.key === "ArrowRight") {movePlayer(playerLocation,"right")}
+
+// console.log(direction);
+})
